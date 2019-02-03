a0b6f3bbf796752fe0f33afa743e8d24636a15ee
SeleznovaSvS <sia_com@ukr.net>
2019-02-03 19:39:33 +0200

    Add first task

---
+++ FirstTask.py
@@ -0,0 +1,28 @@
+print('Hello, Commonlounge!')
+# addition
+print(2 + 3)
+# multiplication
+print(4 * 5)
+# subtraction
+print(5 - 1)
+# division
+print(40 / 2)
+# integer division (fractional part is ignored)
+print(41 // 2)
+# modulo (remainder left after division)
+print(14 % 3)
+# exponent
+print(2 ** 3)
+
+print(-5 + 4)
+
+print((5 + 2) * (7 - 1) / 3)
+
+# (5 + 2) * (7 - 1) / 3
+# => (7) * (7 - 1) / 3
+# => 7 * (7 - 1) / 3
+# => 7 * (6) / 3
+# => 7 * 6 / 3
+# => 42 / 3
+# => 14.0
+
